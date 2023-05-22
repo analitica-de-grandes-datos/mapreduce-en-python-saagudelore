2ca452184e5a01d465ee1a202b9a606f40b0b0fa
saagudelore <saagudelore@unal.edu.co>
2023-05-22 15:43:01 -0500

    P5

--- pregunta_05/mapper.py
+++ pregunta_05/mapper.py
@@ -1,3 +1,12 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+file = sys.stdin
+
+for line in file:
+    words = line.strip().split('   ')
+    date = words[1]
+    month = date.split('-')[1]
+    print(f'{month}\t1')
--- pregunta_05/reducer.py
+++ pregunta_05/reducer.py
@@ -1,3 +1,16 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+file = sys.stdin
+dictionary = {}
+
+for line in file:
+    month = line.strip().split('\t')[0]
+    if month in dictionary:
+        dictionary[month] += 1
+    else:
+        dictionary[month] = 1
+
+print('\n'.join([f'{clave}\t{valor}' for clave, valor in dictionary.items()]))
