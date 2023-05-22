4884daaadd094ff72b5f920b62cd62c86f82b2f1
saagudelore <saagudelore@unal.edu.co>
2023-05-22 16:22:46 -0500

    P7

--- pregunta_07/mapper.py
+++ pregunta_07/mapper.py
@@ -1,3 +1,11 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+file = sys.stdin
+
+for line in file:
+    words = line.strip().split('   ')
+    letter, date, val = words[0], words[1], words[2]
+    print(f'{letter}\t{date}\t{val}')
--- pregunta_07/reducer.py
+++ pregunta_07/reducer.py
@@ -1,3 +1,20 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+file = sys.stdin
+dictionary = {}
+
+for line in file:
+    key, date, val = line.strip().split('\t')
+    val = int(val)
+    if key in dictionary:
+        dictionary[key].append([date, val])
+    else:
+        dictionary[key] = [[date, val]]
+
+for letter in dictionary:
+    dictionary[letter] = sorted(dictionary[letter], key=lambda x: x[1])
+    for lista in dictionary[letter]:
+        print(f'{letter}   {lista[0]}   {lista[1]}')
