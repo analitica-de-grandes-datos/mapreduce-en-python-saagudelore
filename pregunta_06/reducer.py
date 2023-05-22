ecad8f1780997dee9a9d1bd60044edbd3f3d2dd1
saagudelore <saagudelore@unal.edu.co>
2023-05-22 15:56:43 -0500

    P06

--- pregunta_06/reducer.py
+++ pregunta_06/reducer.py
@@ -1,3 +1,18 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+file = sys.stdin
+dictionary = {}
+
+for line in file:
+    letter, val = line.strip().split('\t')
+    val = float(val)
+
+    if letter in dictionary:
+        dictionary[letter] = [max(val,dictionary[letter][0]), min(val,dictionary[letter][1])]
+    else:
+        dictionary[letter] = [val, val]
+
+print('\n'.join([f'{clave}\t{valor[0]}\t{valor[1]}' for clave, valor in dictionary.items()]))
