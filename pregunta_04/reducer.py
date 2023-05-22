74653f25b599f086fe54e909ee9c08aaba70fc79
saagudelore <saagudelore@unal.edu.co>
2023-05-22 15:35:58 -0500

    P4

--- pregunta_04/reducer.py
+++ pregunta_04/reducer.py
@@ -1,3 +1,17 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+file = sys.stdin
+
+dictionary = {}
+for line in file:
+    key, val = line.split('\t')
+
+    if key in dictionary:
+        dictionary[key] += 1
+    else:
+        dictionary[key] = 1
+
+print('\n'.join([f'{clave},{valor}' for clave, valor in dictionary.items()]))
