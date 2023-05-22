ca50285524197de25a717be04ed5288b803384e6
saagudelore <saagudelore@unal.edu.co>
2023-05-22 14:28:34 -0500

    P01

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -1,3 +1,14 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+
+# Emparejar entradas como (clave, valor)
+
+import sys
+
+file = sys.stdin
+
+for line in file:
+    words = line.split(',')
+    credit_history = words[2]
+    print(f"{credit_history}\t1")
