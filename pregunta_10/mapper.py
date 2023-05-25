85021a34c4d2f476bf32369e338091f5f2c5aa13
saagudelore <saagudelore@unal.edu.co>
2023-05-24 20:59:26 -0500

    P10

--- pregunta_10/mapper.py
+++ pregunta_10/mapper.py
@@ -1,3 +1,12 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+file = sys.stdin
+
+for line in file:
+    val, letters = line.strip().split('\t')
+    letters = letters.split(',')
+    for letter in letters:
+        print(f'{letter}\t{val}')
--- pregunta_10/reducer.py
+++ pregunta_10/reducer.py
@@ -1,3 +1,25 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+file = sys.stdin
+
+dictionary = {}
+for line in file:
+    letter, value = line.strip().split('\t')
+    if letter in dictionary:
+        dictionary[letter] += f',{value}'
+    else:
+        dictionary[letter] = value
+
+for clave in dictionary:
+    lista = [int(x) for x in dictionary[clave].split(",")]
+    lista = sorted(lista)
+    dictionary[clave] = ''
+    for letter in lista:
+        if dictionary[clave] == '':
+            dictionary[clave] += f'{letter}'
+        else:
+            dictionary[clave] += f',{letter}'
+    print(f'{clave}\t{dictionary[clave]}')
