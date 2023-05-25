e80e2731c82e45e6628f6545fac5a36d3083569d
saagudelore <saagudelore@unal.edu.co>
2023-05-24 20:32:20 -0500

    P9

--- pregunta_09/mapper.py
+++ pregunta_09/mapper.py
@@ -1,3 +1,10 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+file = sys.stdin
+
+for line in file:
+    letter, date, number = line.strip().split('   ')
+    print(f'{letter}\t{date}\t{number}')
--- pregunta_09/reducer.py
+++ pregunta_09/reducer.py
@@ -1,3 +1,19 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+file = sys.stdin
+
+n = 5
+lista = []
+
+for line in file:
+    letter, date, value = line.strip().split('\t')
+    value = int(value)
+    lista.append([f'{letter}   {date}   {value}', value])
+    
+lista_n = sorted(lista, key=lambda x: x[1])[:6]
+for val in lista_n:
+    print(val[0])
+
